f9e674be96dc3a26733218ae98dc6ad04a17df4e
Chakresh <chakresh.244@gmail.com>
2023-07-25 09:54:43 +0530

    changes done

--- User Registration/FrontEnd/user-registration-fe/src/App.js
+++ User Registration/FrontEnd/user-registration-fe/src/App.js
@@ -1,32 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
-
-function App() {
-  return (
-  <div class="container">
-  <hr/>
-  <h1>User Registration</h1>
-  <hr/>
-
-  <form action="http://localhost:3000/book" method="POST">
-      <div class="form-group">
-          <label for="userId">userId</label>
-          <input class="form-control" name="isbn"/>
-      </div>
-
-      <div class="form-group">
-          <label for="userName">UserName</label>
-          <input class="form-control" name="title"/>
-      </div>
-      <div class="form-group">
-          <label for="password">password</label>
-          <input class="form-control" name="title"/>
-      </div>
-
-      <button type="submit" class="btn btn-primary">Submit</button>
-  </form>
-</div>
-  );
-}
+import React, { useState } from 'react';
 
 export default App;
+
+function App() {
+
+  
+  const [data, setData] = useState({});
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const response = await fetch('http://localhost:4000/userRegistration', {
+      method: 'POST',
+      body: JSON.stringify(data),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    const result = await response.json();
+    console.log(result);
+  }
+
+  return (
+    <div class="container">
+    <hr/>
+    <h1>User Registration</h1>
+    <hr/>
+  
+    <form onSubmit={handleSubmit}>
+        <div class="form-group">
+            <label for="userId">userId</label>
+            <input class="form-control" type = "text" name="userId " onChange={e => setData({...data,userId: e.target.value})}/>
+        </div>
+  
+        <div class="form-group">
+            <label for="userName">UserName</label>
+            <input class="form-control" type = "text" name="userName" onChange={e => setData({...data,userName: e.target.value})}/>
+        </div>
+  
+        <div class="form-group">
+            <label for="password">password</label>
+            <input class="form-control" type = "text" name="password" onChange={e => setData({...data,password: e.target.value})}/>
+        </div>
+  
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+  </div>
+    );
+
+
+
+
+}
